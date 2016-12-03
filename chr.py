17e80b6a84ca7b17f2fb43afeb074556b8a12e34
ritesh-nitjsr <ritesh.nitjsr01@gmail.com>
2016-12-03 19:09:53 +0530

    Input credentials feature added, WebDriverWait Added,Successfully lands into the timeline

--- chr.py
+++ chr.py
@@ -6,37 +6,24 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 
+
+email=raw_input("Enter Email ID/ Phone number:")
+passw=raw_input("Enter Password:")
+
 driver = webdriver.Firefox()
 driver.get("http://www.facebook.com")
 assert "Facebook" in driver.title
 elem = driver.find_element_by_name("email")
 elem.clear()
-elem.send_keys("jsnow5242@gmail.com")
+elem.send_keys(email)
 elem = driver.find_element_by_name("pass")
 elem.clear()
-elem.send_keys("johnsnow007")
+elem.send_keys(passw)
 elem.send_keys(Keys.RETURN)
 delay = 10
-'''
-time.sleep(delay);
-elem=driver.find_element_by_class_name('_2s25').click();
-'''
-
-
 try:
     elem=WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.CLASS_NAME,"_2s25")))
     elem.click();
     print "Successfully Logged in";
 except TimeoutException:
     print "Timeout or wrong email/password";
-    
-'''    
-elem.send_keys(Keys.RETURN)
-elem.clear()
-
-elem=driver.find_element_by_class_name('_s0 _7ql _ry img')
-elem.click()
-
-assert "No results found." not in driver.page_source
-driver.close()
-'''
