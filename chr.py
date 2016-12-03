5084fe6390e2f05dfd7112b83bfc20e2f6c89d5d
ritesh-nitjsr <ritesh.nitjsr01@gmail.com>
2016-12-03 21:31:05 +0530

    Automatically likes the post

--- chr.py
+++ chr.py
@@ -6,9 +6,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 
-
+'''
 email=raw_input("Enter Email ID/ Phone number:")
 passw=raw_input("Enter Password:")
+'''
+email="jsnow5242@gmail.com";
+passw="johnsnow007";
 
 driver = webdriver.Firefox()
 driver.get("http://www.facebook.com")
@@ -20,10 +23,37 @@ elem = driver.find_element_by_name("pass")
 elem.clear()
 elem.send_keys(passw)
 elem.send_keys(Keys.RETURN)
-delay = 10
+delay = 100
+
 try:
     elem=WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.CLASS_NAME,"_2s25")))
+    time.sleep(5);
     elem.click();
     print "Successfully Logged in";
 except TimeoutException:
     print "Timeout or wrong email/password";
+    driver.close();
+
+posts=[]
+try:
+    posts=WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.CLASS_NAME,'userContentWrapper')))
+    posts=driver.find_elements_by_class_name('userContentWrapper')
+    print posts
+    time.sleep(15)
+    for post in posts:
+        post_text=post.find_element_by_xpath(".//div[@class='_5pbx userContent']").text
+        print post_text
+        time.sleep(3)
+        if(post_text.find("day")!=-1 or post_text.find("Day")!=-1 or post_text.find("DAY")!=-1):
+                post.find_element_by_xpath(".//a[@class='UFILikeLink _4x9- _4x9_ _48-k']").click()
+        '''
+    print posts
+    for post in posts:
+        post_text=post.find_element_by_xpath(".//div[@class='_58jw']").text
+        if(post_text=="Birthday"):
+            post.find_element_by_xpath(".//div[@class='UFILikeLink']").click()
+    print "All relevant posts liked"
+    '''
+except TimeoutException:
+    print "Timeout"
+        
