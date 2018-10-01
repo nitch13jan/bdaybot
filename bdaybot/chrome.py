ae77e8574a1dc5aa9d3e9fc508cfae90fe39b0c8
Michaël Lévesque-Dion <michael.levesque-dion.1@ulaval.ca>
2018-10-01 16:35:37 -0400

    Validate Python3, add linter, lint code

--- bdaybot/chrome.py
+++ bdaybot/chrome.py
@@ -5,9 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException   
-
-
 
 driver = webdriver.Chrome()
 driver.get("http://www.facebook.com")
@@ -22,37 +19,50 @@ elem.send_keys(Keys.RETURN)
 delay = 30
 
 try:
-    elem=WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.CLASS_NAME,"_2s25")))
-    time.sleep(5);
-    elem.click();
-    print ("Successfully Logged in")
+    elem = WebDriverWait(driver, delay).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "_2s25")))
+    time.sleep(5)
+    elem.click()
+    print("Successfully Logged in")
 except TimeoutException:
-    print ("Timeout or wrong email/password")
-    driver.close();
+    print("Timeout or wrong email/password")
+    driver.close()
+
 
 def post_checker(post):
-    hpylst = ['happy','hapy','hapi','appy','hpy']
-    bdylst = ['birthday','bday','budday','b\'day','bdy']
-    return any(word in post for word in hpylst) and (any(word in post for word in bdylst) or ('return' in post and 'day' in post))
-posts=[]
+    hpylst = ['happy', 'hapy', 'hapi', 'appy', 'hpy']
+    bdylst = ['birthday', 'bday', 'budday', 'b\'day', 'bdy']
+    has_happy = any(word in post for word in hpylst)
+    has_bday = any(word in post for word in bdylst)
+    has_return_and_day = 'return' in post and 'day' in post
+    return has_happy and (has_bday or has_return_and_day)
+
+
+posts = []
 try:
-    elem=WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.CLASS_NAME,"_44b2")))
-    time.sleep(5);
-    elem.click();
-    posts=WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.CLASS_NAME,'userContentWrapper')))
+    elem = WebDriverWait(driver, delay).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "_44b2")))
+    time.sleep(5)
+    elem.click()
+    posts = WebDriverWait(driver, delay).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'userContentWrapper')))
     time.sleep(8)
-    posts=driver.find_elements_by_class_name('userContentWrapper')
+    posts = driver.find_elements_by_class_name('userContentWrapper')
     for post in posts:
-        post_text=post.find_element_by_xpath(".//div[@class='_5pbx userContent']").text.lower()
+        post_text = post.find_element_by_xpath(
+            ".//div[@class='_5pbx userContent']").text.lower()
         time.sleep(3)
         if post_checker(post_text):
             try:
-                post.find_element_by_xpath(".//a[@class='UFILikeLink _4x9- _4x9_ _48-k']").click()
-            except:
+                post.find_element_by_xpath(
+                    ".//a[@class='UFILikeLink _4x9- _4x9_ _48-k']").click()
+            except Exception as e:
+                print("Could not find element: ")
+                print(e)
                 continue
-    print ("Successfully liked relevant posts")
+    print("Successfully liked relevant posts")
 except TimeoutException:
-    print ("Timeout") 
+    print("Timeout")
 
 app_secret = 'a508a47755cb78460fde5c52c9b9230f'
 app_id = '1178068725594212'
